909af88bd194259260cdf60d40f78868fc458fcc
Alane Alves <alanealvess@gmail.com>
2023-12-14 10:36:37 -0300

    Prática 04 – Parte 3: Adicionando e Testando o Mapa

--- app/src/main/java/pdm/weatherapp/MainNavHost.kt
+++ app/src/main/java/pdm/weatherapp/MainNavHost.kt
@@ -5,9 +5,12 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MainNavHost(navController: NavHostController, viewModel: FavoriteCitiesViewModel, context: Context) {
+    val cameraPositionState = rememberCameraPositionState()
+
     NavHost(navController, startDestination = BottomNavItem.HomePage.route) {
         composable(route = BottomNavItem.HomePage.route) {
             HomePage(viewModel = viewModel, context = context)
@@ -16,7 +19,7 @@ fun MainNavHost(navController: NavHostController, viewModel: FavoriteCitiesViewM
             ListPage(viewModel = viewModel, context = context)
         }
         composable(route = BottomNavItem.MapPage.route) {
-            MapPage(viewModel = viewModel, context = context)
+            MapPage(viewModel = viewModel, context = context, camPosState = cameraPositionState)
         }
     }
 }
--- app/src/main/java/pdm/weatherapp/MapPage.kt
+++ app/src/main/java/pdm/weatherapp/MapPage.kt
@@ -1,33 +1,35 @@
 package pdm.weatherapp
 
+import android.Manifest
 import android.content.Context
-import androidx.activity.ComponentActivity
+import android.content.pm.PackageManager
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 
 @Composable
 fun MapPage(
     modifier: Modifier = Modifier,
     viewModel: FavoriteCitiesViewModel,
-    context: Context
+    context: Context,
+    camPosState: CameraPositionState
 ) {
     Column(
         modifier = Modifier
@@ -35,13 +37,54 @@ fun MapPage(
             .background(colorResource(id = R.color.purple_700))
             .wrapContentSize(Alignment.Center)
     ) {
-        Text(
-            text = "Mapa",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
-        )
+        val recife = LatLng(-8.05, -34.9)
+        val caruaru = LatLng(-8.27, -35.98)
+        val joaopessoa = LatLng(-7.12, -34.84)
+        val hasLocationPermission by remember {
+            mutableStateOf(
+                ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.ACCESS_FINE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED
+            )
+        }
+
+        GoogleMap (
+            modifier = Modifier.fillMaxSize(),
+            onMapClick = { viewModel.add("Nova cidade", location = it) },
+            properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+            uiSettings = MapUiSettings(myLocationButtonEnabled = true),
+            cameraPositionState = camPosState
+        ) {
+            viewModel.cities.forEach {
+                if (it.location != null) {
+                    Marker( state = MarkerState(position = it.location!!),
+                        title = "${it.cityName}", snippet = "${it.location}")
+                }
+            }
+
+            Marker(
+                state = MarkerState(position = recife),
+                title = "Recife",
+                snippet = "Marcador em Recife",
+                icon = BitmapDescriptorFactory.defaultMarker(
+                    BitmapDescriptorFactory.HUE_BLUE)
+            )
+
+            Marker(
+                state = MarkerState(position = caruaru),
+                title = "Caruaru",
+                snippet = "Marcador em Caruaru",
+                icon = BitmapDescriptorFactory.defaultMarker(
+                    BitmapDescriptorFactory.HUE_AZURE)
+            )
+
+            Marker(
+                state = MarkerState(position = joaopessoa),
+                title = "João Pessoal",
+                snippet = "Marcador em João Pessoal",
+                icon = BitmapDescriptorFactory.defaultMarker(
+                    BitmapDescriptorFactory.HUE_GREEN)
+            )
+        }
     }
 }
