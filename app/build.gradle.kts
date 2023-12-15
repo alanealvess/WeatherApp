ab1687b86633ffb50a03e580a275d1159d58b52e
Alane Alves <alanealvess@gmail.com>
2023-12-15 15:37:13 -0300

    Prática 05 – Parte 1: Configuração do Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     // Deve vir depois dos outros plugins (Bug)
     id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -66,6 +67,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.0.1")
     // Google maps for compose
     implementation("com.google.maps.android:maps-compose:2.8.0")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
