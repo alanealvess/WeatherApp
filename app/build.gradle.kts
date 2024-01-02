cad4b75662f88b66257b37b9d1bc7d9867cc7e3f
Alane Alves <alanealvess@gmail.com>
2024-01-02 15:28:57 -0300

    Prática 09 – Parte 0: Preparação e Refatoração

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("io.coil-kt:coil-compose:2.5.0")
     // Google maps
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
@@ -81,6 +83,7 @@ dependencies {
     implementation("com.google.maps.android:maps-compose:2.8.0")
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-firestore:24.10.0")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/pdm/weatherapp/model/FavoriteCity.kt
+++ app/src/main/java/pdm/weatherapp/model/FavoriteCity.kt
@@ -1,5 +1,6 @@
 package pdm.weatherapp.model
 
+import android.graphics.Bitmap
 import com.google.firebase.firestore.Exclude
 import pdm.weatherapp.service.WeatherForecastClasses
 
@@ -7,6 +8,10 @@ data class FavoriteCity (
     var name: String? = null,
     var latitude : Double? = null,
     var longitude : Double? = null,
+    @Exclude
+    var imageUrl : String? = null,
+    @Exclude
+    var bitmap : Bitmap? = null,
     @Exclude
     var currentWeather: WeatherForecastClasses.CurrentWeather? = null,
     @Exclude
